09647c0b850c33e8fb164bf5098ddad41e1db441
elvisior <travis@geelong.com>
2016-01-04 12:24:06 +1100

    Update robot_v3.py
    added discs to simulate the compact discs I want to pick up/put down

--- test_mearm/robot_v3.py
+++ test_mearm/robot_v3.py
@@ -1,3 +1,5 @@
+#!/cygdrive/c/Python27/python
+
 #import required libraries
 from visual import *
 import numpy as np
@@ -15,16 +17,17 @@ pos_z_axis= arrow(pos=(0,0,0), axis=(0,0,-15), shaftwidth=0.1, headwidth=0.3)
 neg_z_axis= arrow(pos=(0,0,0), axis=(0,0,15), shaftwidth=0.1, headwidth=0.3)
 
 #Indicators for the target, link 1 and link 2 respectively
-indicator = arrow(pos=(0,0,0), axis=(10,10,0), shaftwidth=0.2, headwidth=0.3, color=color.yellow)
+#indicator = arrow(pos=(0,0,0), axis=(10,10,0), shaftwidth=0.2, headwidth=0.3, color=color.yellow)
 l1_ind = arrow(pos=(0,0,0), axis=(2.65,7.64,0), shaftwidth=0.2, headwidth=0.3, color=color.red)
 l2_ind = arrow(pos=(2.65,7.64,0), axis=(7.3,2.3,0), shaftwidth=0.2, headwidth=0.3, color=color.green)
 #Labels to show how to move the robot
 label_1=label(pos=(8,18,0), text='Use arrows to move in plane. Use a and d to rotate. Use w and s to open/close the clamp')
 label_2=label(pos=(8,-8,0), text='Clamp status = Close')
 #Labels to improve the visualization of the position of the arm
-in_x_plane=arrow(pos=(0,0,0), axis=(10,0,0), shaftwidth=0.1, headwidth=0.1, color=color.orange, opacity=0.3)
-in_y_plane=arrow(pos=(0,0,0), axis=(0,10,0), shaftwidth=0.1, headwidth=0.1, color=color.orange, opacity=0.3)
-in_z_plane=arrow(pos=(0,0,0), axis=(0,0,10), shaftwidth=0.1, headwidth=0.1, color=color.orange, opacity=0.3)
+in_x_plane=arrow(pos=(0,0,0), axis=(10,0,0), shaftwidth=0.1, headwidth=0.1, color=color.blue, opacity=0.3)
+in_y_plane=arrow(pos=(0,0,0), axis=(0,10,0), shaftwidth=0.1, headwidth=0.1, color=color.cyan, opacity=0.3)
+in_z_plane=arrow(pos=(0,0,0), axis=(0,0,10), shaftwidth=0.1, headwidth=0.1, color=color.green, opacity=0.3)
+
 
 #Initial position
 x=10
@@ -32,25 +35,38 @@ y=10
 phi=0 #angle for base rotation
 clamp = 'Close' #Clamp is close
 
+offset=0.1
+degrees=1
+
+in_x_plane.axis=(x*np.cos(phi*0.01745),0,0)
+in_y_plane.axis=(0,y,0)
+in_z_plane.axis=(0,0,x*np.sin(phi*0.01745))
+print("Initial x,y,z: %.2f,%.2f,%.2f" % (in_x_plane.axis[0], in_y_plane.axis[1], in_z_plane.axis[2]))
+
+#draw initial compact disc
+disc=cylinder(pos=(in_x_plane.axis[0],in_y_plane.axis[1]-3,in_z_plane.axis[2]),axis=(0,0.12,0),radius=6)
+src=cylinder(pos=(1,0,9),axis=(0,0.12,0),radius=6, color=color.green)
+dest=cylinder(pos=(1,0,-9),axis=(0,0.12,0),radius=6, color=color.red)
+
 #now we made an infinite while loop to keep the program running
 while (1==1):
-    rate(20) #refresh rate required for VPython
+    rate(120) #refresh rate required for VPython
     ev = scene.waitfor('keydown')
     if ev.key == 'up':
-        y = y+0.25
+        y = y + offset
     elif ev.key == 'down':
-        y = y-0.25
+        y = y - offset
     elif ev.key == 'right':
-        x = x+0.25
+        x = x + offset
     elif ev.key == 'left':
-        x = x-0.25
+        x = x - offset
     elif ev.key == 'a':
-        phi = phi-5
+        phi = phi-degrees
         if phi <= -90:
             print 'Minimum angle reached'
             phi = -90
     elif ev.key == 'd':
-        phi = phi+5
+        phi = phi+degrees
         if phi >= 90:
             print 'Maximum angle reached'
             phi = 90
@@ -65,6 +81,14 @@ while (1==1):
     elif ev.key == 's':
         print 'Closing clamp...'
         clamp = 'Close'
+    elif ev.key == ']':
+	offset = offset * 2
+	degrees = degrees * 2
+	print "offset = " + repr(offset) + ", degrees = " + repr(degrees)
+    elif ev.key == '[':
+	offset = offset / 2
+	degrees = degrees / 2
+	print "offset = " + repr(offset) + ", degrees = " + repr(degrees)
 
     #Calculate the distance to the target and the angle to the x axis
     T = np.sqrt(x*x+y*y) #Distance to target
@@ -88,7 +112,7 @@ while (1==1):
     ang=3.141592+alpha+theta+beta #Correct angle from the l1 indicator
 
     #Update the indicators
-    indicator.axis=(x*np.cos(phi*0.01745),y,x*np.sin(phi*0.01745)) #calculate the new axis of the indicator
+    #indicator.axis=(x*np.cos(phi*0.01745),y,x*np.sin(phi*0.01745)) #calculate the new axis of the indicator
     l1_ind.axis=(l1*np.cos(alpha+theta)*np.cos(phi*0.01745),l1*np.sin(alpha+theta),l1*np.cos(alpha+theta)*np.sin(phi*0.01745)) #calculate the new axis of l1
     l2_ind.pos=(l1*np.cos(alpha+theta)*np.cos(phi*0.01745),l1*np.sin(alpha+theta),l1*np.cos(alpha+theta)*np.sin(phi*0.01745)) #calculate new origin for l2
     l2_ind.axis=(l2*np.cos(ang)*np.cos(phi*0.01745),l2*np.sin(ang),l2*np.cos(ang)*np.sin(phi*0.01745)) #Calculate new axis for l2
@@ -99,3 +123,5 @@ while (1==1):
     in_y_plane.axis=(0,y,0)
     in_z_plane.axis=(0,0,x*np.sin(phi*0.01745))
     label_2.text='Clamp status = '+clamp
+    disc.pos=(in_x_plane.axis[0],in_y_plane.axis[1]-3,in_z_plane.axis[2])
+    print("x,y,z: %.2f,%.2f,%.2f" % (in_x_plane.axis[0], in_y_plane.axis[1], in_z_plane.axis[2]))
