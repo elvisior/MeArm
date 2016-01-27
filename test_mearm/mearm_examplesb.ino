e11792e22c8a68836654665984fe013de93bbc02
elvisior <travis@geelong.com>
2016-01-27 17:34:31 +1100

    moves smoother now

--- test_mearm/mearm_examplesb.ino
+++ test_mearm/mearm_examplesb.ino
@@ -1,6 +1,7 @@
-
 #include <NewPing.h>
 
+#define VALVE_PIN 13 // Arduino pin tied to trigger pin for the air pump's valve
+#define PUMP_PIN 7   // Arduino pin tied to trigger pin for the air pump
 #define TRIGGER_PIN  12  // Arduino pin tied to trigger pin on the ultrasonic sensor.
 #define ECHO_PIN     3  // Arduino pin tied to echo pin on the ultrasonic sensor.
 #define MAX_DISTANCE 140 // Maximum distance we want to ping for (in centimeters). Maximum sensor distance is rated at 400-500cm.
@@ -15,10 +16,13 @@ Servo zServo;  // create servo object, right side servo - forward backwards moti
 Servo clawServo;  // create servo object, end of arm srevo - open,close the claw hand
  
 //servo positions values, expects 1-180 deg.
+int valve_state=0;
+int pump_state=0;
 int xPos;
 int yPos;
 int zPos;
 int clawPos;
+int pumpPos;
 String location="";
  
 //*************** INIT AT STARTUP *******************************************************************
@@ -30,6 +34,9 @@ void setup() {        // the setup function runs once when you press reset or po
   yServo.attach(10);  // attaches the servo on pin 10 to the servo object
   zServo.attach(9);  // attaches the servo on pin 9 to the servo object
   clawServo.attach(6);  // attaches the servo on pin 6 to the servo object
+
+  pinMode(VALVE_PIN, OUTPUT); //VALVE_PIN
+  pinMode(PUMP_PIN, OUTPUT); //PUMP_PIN
  
   // initialize serial port
   Serial.begin(115200);
@@ -47,28 +54,93 @@ void setup() {        // the setup function runs once when you press reset or po
 // ******************************************************************************************************
  
 void loop() {
- 
-
+  int notthere;
+  int tmpx, tmpy, tmpz, tmpc;
+  
   //serial in packet patern = xVal,yVal,zVal,clawVal + end of packet char '\n'
   while (Serial.available() > 0) {
+    notthere=1;
     xPos = Serial.parseInt();
     yPos = Serial.parseInt();
     zPos = Serial.parseInt();
     clawPos = Serial.parseInt();
+    pumpPos = Serial.parseInt();
  
     if (Serial.read() == '\n') { // Detect end of packet char '\n', go ahead and update servo positions
  
  
       // UPDATE SERVO POSITIONS
+
+      tmpx=xServo.read();
+      tmpy=yServo.read();
+      tmpz=zServo.read();
+      tmpc=clawServo.read();
+      while(notthere){
+        //shuffle x along
+        if(tmpx>xPos){
+          tmpx=tmpx-1;
+        }
+        if(tmpx<xPos){
+          tmpx=tmpx+1;
+        }
+        //shuffle y along
+        if(tmpy>yPos){
+          tmpy=tmpy-1;
+        }
+        if(tmpy<yPos){
+          tmpy=tmpy+1;
+        }  
+        //shuffle z along
+        if(tmpz>zPos){
+          tmpz=tmpz-1;
+        }
+        if(tmpz<zPos){
+          tmpz=tmpz+1;
+        }  
+        //shuffle c along
+        if(tmpc>clawPos){
+          tmpc=tmpc-1;
+        }
+        /*
+        if(tmpc<cPos){
+          tmpc=tmpc+1;
+        }
+        */
+        //check if we are there yet
+        //if(tmpx==xPos && tmpy==yPos && tmpz==zPos && tmpc==clawPos){
+        if(tmpx==xPos && tmpy==yPos && tmpz==zPos){
+          notthere=0;
+        }
+        else{
+          notthere=1;
+        }
+        xServo.write(tmpx);
+        yServo.write(tmpy);
+        zServo.write(tmpz);
+        clawServo.write(tmpc);
+        delay(5);
+      }
+      
       xServo.write(xPos);
       yServo.write(yPos);
       zServo.write(zPos);
       clawServo.write(clawPos);
+      
+      if(pumpPos==1){
+          digitalWrite(VALVE_PIN, LOW);//VALVE_PIN stop
+          digitalWrite(PUMP_PIN, HIGH);//PUMP_PIN work
+      }
+      else{
+          digitalWrite(VALVE_PIN, HIGH);//VALVE_PIN work
+          digitalWrite(PUMP_PIN, LOW);//PUMP_PIN stop        
+      }
       delay(50);
       location = String("Location: " + String(xServo.read()) + "," + String(yServo.read()) + "," + String(zServo.read()) + "," + String(clawServo.read()) + " ");
       Serial.print(location);
       float uS = sonar.ping(); // Send ping, get ping time in microseconds (uS).
       output = String("Ping: " + String(uS / US_ROUNDTRIP_CM) + " cm\n"); // Convert ping time to distance and print result (0 = outside set distance range, no ping echo)
+      Serial.print(output);
+      output = String("\n");
       Serial.print(output);
     }
  
